891db9187234b66ac67e70b82529c574e1955328
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2024-11-03 14:29:31 +0300

    Add About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import About from "./ui/About";
 
 export default function Home() {
   return (
     <div className="p-5">
-      {/* <Header /> */}
+      <About />
     </div>
   );
 }
---
+++ src/app/ui/About.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+// Icons
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const About = () => {
+  return (
+    <section className="mt-10 flex flex-col items-center font-medium">
+      <div className="h-32 w-32 overflow-hidden rounded-full">
+        <img
+          className="size-full object-cover"
+          src="https://plus.unsplash.com/premium_photo-1683140621573-233422bfc7f1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"
+          alt="My photo"
+        />
+      </div>
+      <p className="mt-2 rounded-full bg-white px-4 py-2">Tonny Kimanthi</p>
+      <p className="max-w-lg text-center text-3xl">
+        Bulding Websites, from Frontend to Backend.
+      </p>
+      <div className="mt-2 space-x-2">
+        <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <FaLinkedinIn className="size-6" />
+        </button>
+        <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <FaGithub className="size-6" />
+        </button>
+        <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <FaXTwitter className="size-6" />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default About;
