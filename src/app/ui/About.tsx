a196ed7e956a58401f941247ef22482f2141e68d
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-07 13:15:35 +0300

    Edit the title

--- src/app/ui/About.tsx
+++ src/app/ui/About.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 
+// Images
+import MyImage from "../../../public/my-image.jpg";
+
 // Icons
 import { FaLinkedinIn } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Image from "next/image";
 
 const About = () => {
   return (
     <section className="mt-10 flex flex-col items-center font-medium">
-      <div className="h-32 w-32 overflow-hidden rounded-full max-sm:w-28 max-sm:h-28">
-        <img
+      <div className="h-40 w-40 overflow-hidden rounded-full max-sm:h-28 max-sm:w-28">
+        <Image
+          src={MyImage}
+          alt="My Picture"
           className="size-full object-cover"
-          src="https://plus.unsplash.com/premium_photo-1683140621573-233422bfc7f1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"
-          alt="My photo"
         />
       </div>
       <p className="mt-2 rounded-full bg-white px-4 py-2">Tonny Kimanthi</p>
@@ -20,17 +24,17 @@ const About = () => {
         Bulding Websites, from Frontend to Backend.
       </p>
       <div className="mt-2 space-x-2">
-        <Link href="#">
+        <Link href="https://www.linkedin.com/in/tonnykimanthi/" target="_blank">
           <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
             <FaLinkedinIn className="size-6" />
           </button>
         </Link>
-        <Link href="#">
+        <Link href="https://github.com/Tonnykimanthi" target="_blank">
           <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
             <FaGithub className="size-6" />
           </button>
         </Link>
-        <Link href="#">
+        <Link href="https://x.com/TonyKm_" target="_blank">
           <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
             <FaXTwitter className="size-6" />
           </button>
