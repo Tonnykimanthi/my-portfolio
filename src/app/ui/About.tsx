d7a726d1851a50441ff77f277792dba16ec9049e
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-08-12 18:02:04 +0300

    Add gsap library, Animate Header text on about page

--- src/app/ui/About.tsx
+++ src/app/ui/About.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRef } from "react";
 import Link from "next/link";
 
 // Images
@@ -9,7 +12,22 @@ import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
 
+// Animations
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+gsap.registerPlugin(useGSAP, ScrambleTextPlugin);
+
 const About = () => {
+  const headerText = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(headerText.current, {
+      duration: 1,
+      scrambleText: "Bulding Websites, from Frontend to Backend.",
+    });
+  }, []);
+
   return (
     <section className="mt-10 flex flex-col items-center font-medium text-white">
       <div className="h-40 w-40 overflow-hidden rounded-full max-sm:h-28 max-sm:w-28">
@@ -22,22 +40,25 @@ const About = () => {
       <p className="my-2 rounded-full bg-primary px-4 py-2 text-lg uppercase">
         Tonny Kimanthi
       </p>
-      <p className="max-w-lg text-center text-3xl max-sm:text-2xl">
+      <p
+        className="max-w-lg text-center text-3xl max-sm:text-2xl"
+        ref={headerText}
+      >
         Bulding Websites, from Frontend to Backend.
       </p>
       <div className="mt-2 space-x-2">
         <Link href="https://www.linkedin.com/in/tonnykimanthi/" target="_blank">
-          <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <button className="rounded-full border border-dark bg-dark p-2 text-light transition-all hover:border-light hover:bg-transparent hover:text-light">
             <FaLinkedinIn className="size-6" />
           </button>
         </Link>
         <Link href="https://github.com/Tonnykimanthi" target="_blank">
-          <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <button className="rounded-full border border-dark bg-dark p-2 text-light transition-all hover:border-light hover:bg-transparent hover:text-light">
             <FaGithub className="size-6" />
           </button>
         </Link>
         <Link href="https://x.com/TonyKm_" target="_blank">
-          <button className="rounded-full border border-dark bg-dark p-2 text-light transition hover:bg-transparent hover:text-dark">
+          <button className="rounded-full border border-dark bg-dark p-2 text-light transition-all hover:border-light hover:bg-transparent hover:text-light">
             <FaXTwitter className="size-6" />
           </button>
         </Link>
